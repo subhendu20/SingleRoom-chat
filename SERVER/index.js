8de5eabe241f95c5007fcbac0971cf1652b752c7
subhendu20 <64896535+subhendu20@users.noreply.github.com>
2023-04-13 16:02:45 +0530

    new commit

--- SERVER/index.js
+++ SERVER/index.js
@@ -1,55 +1,38 @@
-const cors=require('cors')
+const cors = require('cors')
 cors()
-const io = require('socket.io')(7001,{  cors: {
-          origin: "http://127.0.0.1:5500",
-          methods: ["GET", "POST"],
-          transports: ['websocket', 'polling'],
-          credentials: true
-      },
-      allowEIO3: true
+const io = require('socket.io')(7001, {
+  cors: {
+    origin: "http://127.0.0.1:5500",
+    methods: ["GET", "POST"],
+    transports: ['websocket', 'polling'],
+    credentials: true
+  },
+  allowEIO3: true
+})
+const names = {}
+io.on("connection", (socket) => {
+  socket.on("new-user", (name) => {
+    // world
+    names[socket.id] = name;
+
+
+
+    console.log(names)
+
+
+    socket.broadcast.emit("new-one", names[socket.id])
+
+  });
+  socket.on('send', (message) => {
+    if (names[socket.id] !== undefined) {
+      socket.broadcast.emit('receive', { message: message, user: names[socket.id] })
+
+    }
+  })
+  socket.on('disconnect', (name) => {
+    socket.broadcast.emit('left', name)
+
   })
 
 
-
-const names={}
-
-io.on("connection", (socket) => {
-  
-
-          
-          socket.on("new-user", (name) => {
-             // world
-             names[socket.id]=name;
-             
-           
-           
-            console.log(names)
-            
-            
-            socket.broadcast.emit("new-one",names[socket.id])
-            
-          });
-          socket.on('send',(message)=>{
-            if(names[socket.id]!==undefined){
-              socket.broadcast.emit('receive',{message:message,user:names[socket.id]})
-
-            }
-           
-            
-            
-              
-
-
-            
-          
-             
-          
-            
-          })
-          socket.on('disconnect',(name)=>{
-            socket.broadcast.emit('left',name)
-            
-          })
-          
-          
-        })
+})
